a748979aba5de5adbef4110111d978f1d143a40e
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-09-29 14:22:29 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,1 +1,89 @@
+//{ Driver Code Starts
+// Initial Template for Java
 
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(in.readLine());
+        while (T-- > 0) {
+            String s[] = in.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int a[][] = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                s = in.readLine().trim().split(" ");
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = Integer.parseInt(s[j]);
+                }
+            }
+            Solution ob = new Solution();
+            out.println(ob.numberOfEnclaves(a));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+   public  int numberOfEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        
+        for (int i = 0; i < n; i++) {
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0);
+            }
+            if (grid[i][m - 1] == 1) {
+                dfs(grid, i, m - 1);
+            }
+        }
+        for (int j = 0; j < m; j++) {
+            if (grid[0][j] == 1) {
+                dfs(grid, 0, j);
+            }
+            if (grid[n - 1][j] == 1) {
+                dfs(grid, n - 1, j);
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int[][] grid, int x, int y) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) {
+            return;
+        }
+
+        grid[x][y] = 0; 
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y + 1);
+        dfs(grid, x, y - 1);
+    }
+}
