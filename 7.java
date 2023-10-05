1129df3aae4f3704c10025cb8e931aad0b6f00fb
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-10-05 17:24:43 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+       int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 3, 3, 2, 2, 1, 1, 1};
+        List<Integer> result = solution.majorityElement(nums);
+        System.out.println(result); 
+    }
+}
