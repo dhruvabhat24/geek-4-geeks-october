c9aeb7db2f30a2e12da9ff82d4480c0f75ad8607
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-10-06 10:41:24 +0530

    Create 8.java

--- 8.java
+++ 8.java
@@ -1,1 +1,94 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
 
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Rearr
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  =sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                temp.next = new Node(a);
+                temp = temp.next;
+            }
+            
+            Solution ob = new Solution();
+            ob.rearrange(head);
+            printLast(head);
+            System.out.println();
+        }
+    }
+    
+    public static void printLast(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class Solution
+{
+    public static void rearrange(Node odd)
+
+    {
+        if (odd == null || odd.next == null) {
+            return;
+        }
+
+        Node tail = odd;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+   
+        Node curr = odd;
+        Node nextNode = odd.next;
+
+        while (curr != tail && nextNode != tail) {
+            curr.next = nextNode.next;
+            nextNode.next = tail.next;
+            tail.next = nextNode;
+
+            curr = curr.next;
+            nextNode = curr.next;
+        }
+    
+    }
+}
