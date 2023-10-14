3b9161af6d757b44fde9e362297af714bdab9030
Dhruva Bhat S N <dhruvabhat24@gmail.com>
2023-10-14 09:12:55 +0530

    Create 15.java

---
+++ 15.java
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node{
+    int data;
+    Node left, right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution
+{
+    private static HashSet<Integer> hset; //used to store values from bst
+    private static ArrayList<Integer> list;//it store coommon values
+    private static boolean traverse;
+    
+    public static ArrayList<Integer> findCommon(Node root1,Node root2)
+    {
+        //code here
+        hset = new HashSet<>(); //initialize the hashset
+        list = new ArrayList<>();//initialize the arraylist
+        traverse = false ;
+        inorder(root1);//traverse the first bst
+        traverse = true;
+        inorder(root2); // traverse the second bst
+        return list;//return the list of common values
+        
+    }
+    //helper function to perform an in-order traversa; of bst
+    private static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        
+        if(!traverse){
+            hset.add(root.data); //add values to the hashset from first bst
+        }else{
+            if(hset.contains(root.data)){
+                list.add(root.data);
+                //if value is in hashset ,it's a coommon value ,so add it to the list
+            }
+        }
+        inorder(root.right);
+        return;
+    }
+}
+
+
+//{ Driver Code Starts.
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the current node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the current node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the current node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root1 = buildTree(s);
+            
+            s = br.readLine();
+            Node root2 = buildTree(s);
+            
+            Solution g = new Solution();
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            res = g.findCommon(root1,root2);
+            for(int i = 0;i<res.size();i++)
+                System.out.print(res.get(i) + " ");
+            System.out.println();
+            
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
